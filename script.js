ef9a8d1b5a6f322610a01627b27b58f3d3ea192a
Tupapvzyzz <140374611+Tupapvzyzz@users.noreply.github.com>
2026-07-02 19:15:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -18,14 +18,11 @@ window.addEventListener("load", () => {
 
 
 /* ================= CURSOR ================= */
-
 const cursor = document.querySelector(".cursor");
 
 document.addEventListener("mousemove", (e) => {
-
     cursor.style.left = e.clientX + "px";
     cursor.style.top = e.clientY + "px";
-
 });
 
 
