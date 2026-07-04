2eae181396ec0bba30956540a797000296e106f9
Tupapvzyzz <140374611+Tupapvzyzz@users.noreply.github.com>
2026-07-03 20:01:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-/* ================= LOADER ================= */
+/* ================= ENTRADA MECAMOTOR ================= */
+
 window.addEventListener("load", () => {
   const loader = document.getElementById("loader");
 
@@ -6,10 +7,10 @@ window.addEventListener("load", () => {
     setTimeout(() => {
       loader.style.opacity = "0";
       loader.style.visibility = "hidden";
-    }, 1200);
+      loader.style.pointerEvents = "none";
+    }, 4200);
   }
 });
-
 /* ================= CURSOR KAWASAKI PRO ================= */
 const cursor = document.getElementById("kawaCursor");
 
